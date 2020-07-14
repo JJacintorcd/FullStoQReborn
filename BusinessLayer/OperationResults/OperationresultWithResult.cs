3402cc6f6ec08b7bf18c27ac1a552f95afea2df8
João Jacinto <jjacinto@recodme.pt>
2020-07-14 19:31:23 +0100

    first commit

---
+++ BusinessLayer/OperationResults/OperationresultWithResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recodme.RD.FullStoQReborn.BusinessLayer.OperationResults
+{
+    public class OperationResult<T> : OperationResult
+    {
+        public T Result { get; set; }
+    }
+}
---
+++ DataAccessLayer/Contexts/Context.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Recodme.RD.FullStoQReborn.DataAccessLayer.Properties;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recodme.RD.FullStoQReborn.DataAccessLayer.Contexts
+{
+    public class Context : IdentityDbContext
+    {
+        public Context() : base()
+        {
+
+        }
+
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Resources.ConnectionString);
+            }
+        }
+
+
+    }
+}
