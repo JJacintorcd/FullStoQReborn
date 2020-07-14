f1d56a2d13b458e56254d1129c4674d6674bb05b
v-vilhena <61181510+v-vilhena@users.noreply.github.com>
2020-07-14 20:33:47 +0100

    Added Tests - Profile, Category

--- DataAccessLayer/Contexts/Context.cs
+++ DataAccessLayer/Contexts/Context.cs
@@ -2,7 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using Recodme.RD.FullStoQReborn.DataAccessLayer.Properties;
 using Recodme.RD.FullStoQReborn.DataLayer.Commercial;
-using Recodme.RD.FullStoQReborn.DataLayer.Person;
 using System;
 using System.Collections.Generic;
 using System.Text;
