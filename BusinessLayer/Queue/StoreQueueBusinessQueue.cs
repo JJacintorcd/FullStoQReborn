d684d18c192b201151139ef7b25cb71450621d97
v-vilhena <61181510+v-vilhena@users.noreply.github.com>
2020-07-14 23:26:57 +0100

    Added StoreQueue, Updated ContextSeeder + Context

---
+++ BusinessLayer/Queue/StoreQueueBusinessQueue.cs
@@ -0,0 +1,316 @@
+﻿using Recodme.RD.FullStoQReborn.BusinessLayer.OperationResults;
+using Recodme.RD.FullStoQReborn.DataAccessLayer.Queue;
+using Recodme.RD.FullStoQReborn.DataLayer.Queue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Recodme.RD.FullStoQReborn.BusinessLayer.Queue
+{
+    public class StoreQueueBusinessObject
+    {
+        private readonly StoreQueueDataAccessObject _dao;
+
+        public StoreQueueBusinessObject()
+        {
+            _dao = new StoreQueueDataAccessObject();
+
+        }
+
+        #region C
+
+        public OperationResult Create(StoreQueue item)
+        {
+            try
+            {
+                _dao.Create(item);
+                return new OperationResult() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+
+            }
+
+        }
+        public async Task<OperationResult> CreateAsync(StoreQueue item)
+        {
+            try
+            {
+                await _dao.CreateAsync(item);
+                return new OperationResult() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+
+            }
+
+        }
+
+        #endregion
+
+        #region R
+
+        public OperationResult<StoreQueue> Read(Guid id)
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+
+                };
+                using var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+                var result = _dao.Read(id);
+                transactionScope.Complete();
+                return new OperationResult<StoreQueue>() { Success = true, Result = result };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult<StoreQueue>() { Success = false, Exception = e };
+
+            }
+
+
+        }
+
+        public async Task<OperationResult<StoreQueue>> ReadAsync(Guid id)
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+
+                };
+                using var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+                var result = await _dao.ReadAsync(id);
+                transactionScope.Complete();
+                return new OperationResult<StoreQueue>() { Success = true, Result = result };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult<StoreQueue>() { Success = false, Exception = e };
+
+            }
+
+        }
+
+        #endregion
+
+        #region U
+
+        public OperationResult Update(StoreQueue item)
+        {
+            try
+            {
+                _dao.Update(item);
+                return new OperationResult() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+
+            }
+
+        }
+        public async Task<OperationResult> UpdateAsync(StoreQueue item)
+        {
+            try
+            {
+                await _dao.UpdateAsync(item);
+                return new OperationResult() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+
+            }
+
+        }
+
+        #endregion
+
+        #region D
+
+        public OperationResult Delete(StoreQueue item)
+        {
+            try
+            {
+                _dao.Delete(item);
+                return new OperationResult() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+
+            }
+
+        }
+
+        public async Task<OperationResult> DeleteAsync(Guid id)
+        {
+            try
+            {
+                await _dao.DeleteAsync(id);
+                return new OperationResult() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+
+            }
+
+        }
+        public OperationResult Delete(Guid id)
+        {
+            try
+            {
+                _dao.Delete(id);
+                return new OperationResult() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+
+            }
+
+        }
+        public async Task<OperationResult> DeleteAsync(StoreQueue item)
+        {
+            try
+            {
+                await _dao.DeleteAsync(item);
+                return new OperationResult() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+
+            }
+
+        }
+
+        #endregion
+
+        #region L
+
+        public OperationResult<List<StoreQueue>> List()
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+                };
+
+                using var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+                var result = _dao.List().Where(x => !x.IsDeleted).ToList();
+                transactionScope.Complete();
+
+                return new OperationResult<List<StoreQueue>>() { Success = true, Result = result };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult<List<StoreQueue>>() { Success = false, Exception = e };
+
+            }
+
+        }
+        public async Task<OperationResult<List<StoreQueue>>> ListAsync()
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+                };
+
+                using var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+                var res = await _dao.ListAsync();
+                var result = res.Where(x => !x.IsDeleted).ToList();
+                transactionScope.Complete();
+
+                return new OperationResult<List<StoreQueue>>() { Success = true, Result = result };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult<List<StoreQueue>>() { Success = false, Exception = e };
+
+            }
+        }
+
+        #endregion
+
+        #region LUD
+        public OperationResult<List<StoreQueue>> ListNotDeleted()
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+                };
+
+                using var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+                var result = _dao.List().Where(x => !x.IsDeleted).ToList();
+
+                transactionScope.Complete();
+
+                return new OperationResult<List<StoreQueue>>() { Success = true, Result = result };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult<List<StoreQueue>>() { Success = false, Exception = e };
+
+            }
+
+        }
+        public async Task<OperationResult<List<StoreQueue>>> ListNotDeletedAsync()
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+                };
+
+                using var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+                var res = await _dao.ListAsync();
+                var result = res.Where(x => !x.IsDeleted).ToList();
+                transactionScope.Complete();
+                return new OperationResult<List<StoreQueue>>() { Success = true, Result = result };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult<List<StoreQueue>>() { Success = false, Exception = e };
+
+            }
+        }
+        #endregion
+    }
+}
---
+++ Dat/Queue/StoreQueue.cs
@@ -0,0 +1,43 @@
+﻿using Recodme.RD.FullStoQReborn.Data.Base;
+using Recodme.RD.FullStoQReborn.DataLayer.Commercial;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Recodme.RD.FullStoQReborn.DataLayer.Queue
+{
+    public class StoreQueue : Entity
+    {
+        private int _quantity;
+
+        [Required]
+        public int Quantity
+        {
+            get => _quantity;
+            set
+            {
+                _quantity = value;
+                RegisterChange();
+            }
+        }
+
+        [ForeignKey("Establishment")]
+        public Guid EstablishmentId { get; set; }
+        public virtual Establishment Establishment { get; set; }
+
+        public StoreQueue(int quantity, Guid establishmentId) : base()
+        {
+            Quantity = quantity;
+            EstablishmentId = establishmentId;
+        }
+
+        public StoreQueue(Guid id, DateTime createdAt, DateTime updatedAt, bool isDeleted, int quantity, Guid establishmentId) : base
+            (id, createdAt, updatedAt, isDeleted)
+        {
+            Quantity = quantity;
+            EstablishmentId = establishmentId;
+        }
+    }
+}
