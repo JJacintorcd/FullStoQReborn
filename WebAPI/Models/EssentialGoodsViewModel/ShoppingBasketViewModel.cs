7034b02a38b8fa4a1f40fb48e57e6b31eb0fb167
v-vilhena <61181510+v-vilhena@users.noreply.github.com>
2020-07-15 21:53:29 +0100

    Updated Person and EssentialGoods VM

--- WebAPI/Models/EssentialGoodsViewModel/ShoppingBasketViewModel.cs
+++ WebAPI/Models/EssentialGoodsViewModel/ShoppingBasketViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Recodme.RD.FullStoQReborn.DataLayer.EssentialGoods;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,18 @@ namespace WebAPI.Models.EssentialGoodsViewModel
 {
     public class ShoppingBasketViewModel
     {
+        public Guid ProfileId { get; set; }
+        public static ShoppingBasketViewModel Parse(ShoppingBasket shoppingBasket)
+        {
+            return new ShoppingBasketViewModel()
+            {
+                ProfileId = shoppingBasket.ProfileId
+            };
+        }
+
+        public ShoppingBasket ToShoppingBasket()
+        {
+            return new ShoppingBasket(ProfileId);
+        }
     }
 }
