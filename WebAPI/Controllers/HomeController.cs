d6fb37dc688ba78e21259e0120d0566219caa13b
v-vilhena <61181510+v-vilhena@users.noreply.github.com>
2020-07-23 16:26:33 +0100

    Added Administration Page

--- WebAPI/Controllers/HomeController.cs
+++ WebAPI/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using WebAPI.Models;
@@ -32,6 +33,12 @@ namespace WebAPI.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        
+        //[Authorize(Roles = "Admin")]
+        public IActionResult Administration()
+        {
+            return View();
         }
     }
 }
