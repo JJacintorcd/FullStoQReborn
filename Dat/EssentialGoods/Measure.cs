6d300474bb7a3e02a94c5da2e008bc7cc4020364
v-vilhena <61181510+v-vilhena@users.noreply.github.com>
2020-07-23 19:47:57 +0100

    Altered ProductModel Data, ViewModel,

---
+++ Dat/EssentialGoods/Measure.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recodme.RD.FullStoQReborn.DataLayer.EssentialGoods
+{
+    public enum Measure
+    {
+        Kg,
+        g,
+        L,
+        mL
+    }
+}
